e48184dad9ac8fcffef8b4b710d5c3f815bf71d3
Michael <michael.gasser@hftm.ch>
2026-06-04 20:26:35 +0200

    feat: implement initial FeedingSchedule component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FeedingSchedule } from './components/feeding-schedule/feeding-schedule';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FeedingSchedule },
+];
